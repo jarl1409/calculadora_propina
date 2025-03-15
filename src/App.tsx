3f7b116f64c0c0ab3b48f28e39523c2f973846e6
jarl1409 <jhosepandrey203@gmail.com>
2025-03-15 01:08:47 -0500

    instalando tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-9xl text-teal-600 font-black">Hola mundo</h1>
+      <p className="text-5xl font-light">Provando</p>
+    </>
+  )
+}
+
+export default App
